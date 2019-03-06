9ed443dfaebd688f900b07cf0d29b588816856c2
Richard Hu <46357823+rzhanghu64@user.noreply.github.com>
2019-03-05 19:06:59 -0500

    added b540421 commit to github.op

---
+++ projects/api-project-1/assets/javascript/basic-search.js
@@ -0,0 +1,54 @@
+// Genre - API result
+
+$.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    crossDomain: true,
+    jsonp: 'json_callback',
+    url: 'http://www.giantbomb.com/api/genres/?format=jsonp&api_key=3e367e43b48af015b21cb7640630f3fa0e510098'
+}).done(function (response) {
+    console.log(response);
+    var results = response.results;
+    for (i = 0; i < results.length; i++) {
+        var formOption = $("<option>")
+        formOption.text(results[i].name);
+        formOption.attr("data-guid", results[i].guid);
+        $("#search-genre").append(formOption);
+    }
+}).fail(function () {
+    alert("ajax error");
+}).always(function () {
+    alert("ajax complete");
+});
+
+
+// Platforms - API result
+
+$.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    crossDomain: true,
+    jsonp: 'json_callback',
+    url: 'http://www.giantbomb.com/api/platforms/?format=jsonp&api_key=3e367e43b48af015b21cb7640630f3fa0e510098'
+}).done(function (response) {
+    console.log(response);
+    var results = response.results;
+    for (i = 0; i < results.length; i++) {
+        var formOption1 = $("<option>")
+        formOption1.text(results[i].name);
+        formOption1.attr("data-guid1", results[i].guid);
+        $("#search-platform").append(formOption1);
+    }
+}).fail(function () {
+    alert("ajax error");
+}).always(function () {
+    alert("ajax complete");
+});
+
+//*thoughts and ideas
+//make search based on name OR genre?
+
+//logic flow for search based on specific game
+//user enters specific game -> redirect to list of possible games (results html page) 
+//-> get game guid from api -> use object 'related' in array
+//-> display recommendations, possibly cycle through them using bootstrap carousel or modals?
--- projects/api-project-1/assets/javascript/logic.js
+++ projects/api-project-1/assets/javascript/logic.js
@@ -1,3 +1,4 @@
+async function initialCall(){
 axios({
     url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
     method: 'POST',
@@ -7,30 +8,49 @@ axios({
     },
 
     //https://api-docs.igdb.com/?javascript#examples-12 and https://api-docs.igdb.com/?javascript#game
-    data: "fields *; where genres = (8) & platforms = 48;"
+    data: "fields *; where genres = (12) & platforms = 48;"
 })
-    .then(response => {
+    .then(async response => {
         var results = response.data;
         console.log(response.data);
         for (var i = 0; i < 4; i++) {
-            var gameID = results[i].id;
             var cardDiv = $('<div>');
             cardDiv.addClass("card");
-            var name = results[i].name;
-            var h = $("<h5>").text(name);
+            cardDiv.attr("id", "card-div-"+i)
+            cardDiv.addClass("col-3");
+            var h = $("<h5>").text(results[i].name);
             h.addClass("card-title");
+            var p = $("<p>").text(results[i].summary)
             var img = $("<img>");
             img.addClass("game-image");
-            console.log(getCover(gameID));
-            img.attr("src", getCover(gameID));
+            img.attr("id", "game-image-"+i)
+            img.attr("data-gameid", results[i].id);
+            //await getCover(gameID);
+            //console.log(imgLocation);
+            img.attr("src", "https://via.placeholder.com/100");
             cardDiv.append(img);
             cardDiv.append(h);
-            $("#card-container").prepend(cardDiv);
+            cardDiv.append(p);
+            $("#card-container").append(cardDiv);
         }
+        await fetchCovers();
     })
     .catch(err => {
         console.error(err);
     });
+};
+initialCall();
+
+async function fetchCovers(){
+    for (var i = 0; i < 1; i++) {
+        var img = $("#game-image-"+i);
+        var currentGameID = img.data("gameid");
+        let imgsrc = await getCover(currentGameID);
+        console.log(imgsrc);
+        img.attr("src", imgsrc);
+        $("#card-div-"+i).prepend(img);
+    }
+};
 
 function getCover(id) {
     axios({
@@ -45,8 +65,9 @@ function getCover(id) {
         data: "fields *; where game = " + id + ";"
     })
         .then(response => {
-            console.log(response.data);
             var results = response.data;
+            console.log(results[0].url);
+            imgsrc = results[0].url;
             return results[0].url;
         })
         .catch(err => {
