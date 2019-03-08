3648c3a9a3f6d1e4972c5f77b092c9d51e8a1a87
Richard Hu <46357823+rzhanghu64@user.noreply.github.com>
2019-03-08 18:46:41 -0500

    fixed https

--- projects/api-project-1/assets/javascript/basic-search.js
+++ projects/api-project-1/assets/javascript/basic-search.js
@@ -22,7 +22,49 @@ $.ajax({
 //search for a specific game
 $(document).ready(function () {
     $("#input-submit").click(function () {
-        console.log("start submit");
+        console.log("clicked");
+        var gameInput = $("#input-keyword").val();
+        $.ajax({
+            type: 'GET',
+            dataType: 'jsonp',
+            crossDomain: true,
+            jsonp: 'json_callback',
+            url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key=3e367e43b48af015b21cb7640630f3fa0e510098',
+            data: {
+                "query": gameInput,
+                "resources": "game",
+            },
+        }).done(function (response) {
+            console.log(response);
+            $("#result-list-container").empty();
+            var resultHeader = $("<h3>")
+            resultHeader.text("Quick Results");
+            $("#result-list-container").prepend(resultHeader);
+            var results = response.results;
+            for (i = 0; i < results.length; i++) {
+                var gameDiv = $("<div>");
+                var h = $("<h4>");
+                h.text(results[i].name);
+                gameDiv.attr("data-guid", results[i].guid);
+                gameDiv.click(loadGamePage);
+                gameDiv.append(h);
+                $("#result-list-container").append(gameDiv);
+            }
+        }).fail(function () {
+            alert("ajax error");
+        })
+    });
+});
+
+function loadGamePage() {
+    localStorage.setItem("guid", $(this).data("guid"));
+    window.location = "results.html";
+};
+
+$("#input-keyword").keyup(function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        console.log("pressed enter");
         var gameInput = $("#input-keyword").val();
         $.ajax({
             type: 'GET',
@@ -50,10 +92,5 @@ $(document).ready(function () {
         }).fail(function () {
             alert("ajax error");
         })
-    });
+    }
 });
-
-function loadGamePage() {
-    localStorage.setItem("guid", $(this).data("guid"));
-    window.location = "results.html";
-};
